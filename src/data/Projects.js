505f8673351f7b3341a0d29d2264bb56ced55062
ahmed mhmoud abdelaal <ahmedmhmouad41@gmail.com>
2024-09-24 21:54:51 +0300

    after realy last commit

---
+++ src/data/Projects.js
@@ -0,0 +1,51 @@
+import HOOBANK from '../assets/Hoo-Bank.jpeg'
+import ROCKETACME from '../assets/Rocket_Acme.jpeg'
+import SMS from '../assets/SMS.png'
+import Scopedia from '../assets/Scopedia.png'
+import FChat from '../assets/Chat-Ease.png'
+import HeartCare from '../assets/Heart-care.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"Hoo Bank Portfolio",
+        image:HOOBANK,
+        github:"https://github.com/ahmed-226/HooBank",
+        live:"",
+    },
+    {
+        id:2,
+        name:"Froggy Chat",
+        image:FChat,
+        github:"https://github.com/ahmed-226/Chat-Ease",
+        live:"",
+    },
+    {
+        id:3,
+        name:"Acme Rockets",
+        image:ROCKETACME,
+        github:"https://github.com/ahmed-226/Acme-Rockets",
+        live:"https://acme-rockets-4k06.onrender.com/",
+    },
+    {
+        id:4,
+        name:"Shcool Management System",
+        image:SMS,
+        github:"https://github.com/ahmed-226/School_mangment_system",
+        live:"",
+    },
+    {
+        id:5,
+        name:"Scoipedia",
+        image:Scopedia,
+        github:"https://github.com/ahmed-226/Sociopedia",
+        live:"https://sociopedia-client-ztmd.onrender.com/",
+    },
+    {
+        id:6,
+        name:"Heart Care",
+        image:HeartCare,
+        github:"https://github.com/sbme-tutorials/his-finalproject-database_sbe-_spring24_team1",
+        live:"",
+    }
+]
---
+++ src/data/Skills.js
@@ -0,0 +1,69 @@
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import JS from '../assets/javascript.png'
+import ReactIcon from '../assets/react.png'
+import NodeIcon from '../assets/node.png'
+import MongoIcon from '../assets/mongo.png'
+import TailwindIcon from '../assets/tailwind.png'
+import GithubIcon from '../assets/github.png'
+import PostgresIcon from '../assets/postgres.png'
+import ExpressIcon from '../assets/express.png'
+import MaterialUIIcon from '../assets/MUI.png'
+
+export const data = [
+  {
+    id: 1,
+    image: HTML,
+    name: 'HTML',
+  },
+  {
+    id: 2,
+    image: CSS,
+    name: 'CSS',
+  },
+  {
+    id: 3,
+    image: JS,
+    name: 'JavaScript',
+  },
+  {
+    id: 4,
+    image: ReactIcon,
+    name: 'React',
+  },
+  {
+    id: 5,
+    image: NodeIcon,
+    name: 'Node.js',
+  },
+  {
+    id: 6,
+    image: MongoIcon,
+    name: 'MongoDB',
+  },
+  {
+    id: 7,
+    image: TailwindIcon,
+    name: 'Tailwind CSS',
+  },
+  {
+    id: 8,
+    image: GithubIcon,
+    name: 'GitHub',
+  },
+  {
+    id: 9,
+    image: PostgresIcon,
+    name: 'PostgreSQL',
+  },
+  {
+    id: 10,
+    image: ExpressIcon,
+    name: 'Express.js',
+  },
+  {
+    id: 11,
+    image: MaterialUIIcon,
+    name: 'Material-UI',
+  },
+];
